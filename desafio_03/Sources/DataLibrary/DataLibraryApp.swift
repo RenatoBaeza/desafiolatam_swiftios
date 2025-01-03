24fc9d87b24b5c57213ec6bbed4ed07db2eca651
RenatoBaeza <rntbzbg@gmail.com>
2025-01-03 00:13:44 -0300

    portrait only

--- desafio_03/Sources/DataLibrary/DataLibraryApp.swift
+++ desafio_03/Sources/DataLibrary/DataLibraryApp.swift
@@ -6,6 +6,15 @@ struct DataLibraryApp: App {
     let container: ModelContainer
     
     init() {
+        // Set supported orientations to portrait only
+        UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue, forKey: "orientation")
+        
+        // Disable landscape orientations
+        if #available(iOS 16.0, *) {
+            UIApplication.shared.connectedScenes.compactMap { ($0 as? UIWindowScene)?.keyWindow }.last?.rootViewController?.setNeedsUpdateOfSupportedInterfaceOrientations()
+        }
+        
+        // Initialize ModelContainer
         do {
             let config = ModelConfiguration(isStoredInMemoryOnly: true)
             container = try ModelContainer(for: UserElement.self, configurations: config)
@@ -19,6 +28,13 @@ struct DataLibraryApp: App {
             MainView()
         }
         .modelContainer(container)
+    }
+}
+
+// Add this extension to enforce portrait orientation
+extension UIWindowScene {
+    func requestGeometryUpdate(using preferences: UIWindowScene.GeometryPreferences) async throws {
+        try await self.requestGeometryUpdate(preferences)
     }
 }
 
